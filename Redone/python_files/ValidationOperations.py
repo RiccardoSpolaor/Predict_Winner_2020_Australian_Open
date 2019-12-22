75b1e0fd87d7cd31c4b3827502078a127281f0ff
ricca <riccardo.spolaor94@gmail.com>
2019-12-22 19:40:07 +0100

    Fixed Bias Var Decomposition

--- Redone/python_files/ValidationOperations.py
+++ Redone/python_files/ValidationOperations.py
@@ -9,6 +9,178 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
 
 
+def get_tree_variance_bias_decomposition(dataframe):
+    n_tests = 5
+    stats = np.array([])
+
+    x_train, x_valid, y_train, y_valid = train_test_split(dataframe.loc[:, dataframe.columns != 'Winner'],
+                                                          dataframe['Winner'], test_size=0.33, shuffle=False,
+                                                          stratify=None)
+
+    depth = range(2, 20)
+    for n in depth:
+        y_preds = np.array([])
+
+        for i in range(n_tests):
+            xs, ys = resample(x_train, y_train, n_samples=int(0.67 * len(y_train)))
+
+            # train a decision tree classifier
+            estimator = tree.DecisionTreeClassifier(max_depth=n)
+            estimator.fit(xs, ys)
+
+            y_pred = estimator.predict(x_valid)
+            y_preds = np.column_stack([y_preds, y_pred]) if y_preds.size else y_pred
+
+        est_bias = (y_valid - np.mean(y_preds, axis=1)) ** 2
+        est_variance = np.var(y_preds, axis=1)
+        est_error = (y_preds - y_valid.values.reshape(-1, 1)) ** 2
+
+        run_stats = np.array([est_error.mean(), est_bias.mean(), est_variance.mean()])
+
+        stats = np.column_stack([stats, run_stats]) if stats.size else run_stats
+
+    plot_estimator_variance_bias_decomposition(depth, stats, 'Depth')
+
+
+def get_bagged_tree_variance_bias_decomposition(dataframe, dt):
+    n_tests = 3
+    stats = np.array([])
+
+    x_train, x_valid, y_train, y_valid = train_test_split(dataframe.loc[:, dataframe.columns != 'Winner'],
+                                                          dataframe['Winner'], test_size=0.33, shuffle=False,
+                                                          stratify=None)
+
+    n_estimators = range(50, 301, 25)
+    for n in n_estimators:
+        y_preds = np.array([])
+
+        for i in range(n_tests):
+            xs, ys = resample(x_train, y_train, n_samples=int(0.67 * len(y_train)))
+
+            # train a decision tree classifier
+            estimator = BaggingClassifier(dt, n_estimators=n, n_jobs=-1)
+            estimator.fit(xs, ys)
+
+            y_pred = estimator.predict(x_valid)
+            y_preds = np.column_stack([y_preds, y_pred]) if y_preds.size else y_pred
+
+        est_bias = (y_valid - np.mean(y_preds, axis=1)) ** 2
+        est_variance = np.var(y_preds, axis=1)
+        est_error = (y_preds - y_valid.values.reshape(-1, 1)) ** 2
+
+        run_stats = np.array([est_error.mean(), est_bias.mean(), est_variance.mean()])
+
+        stats = np.column_stack([stats, run_stats]) if stats.size else run_stats
+
+    plot_estimator_variance_bias_decomposition(n_estimators, stats, 'Estimators')
+
+
+def get_boosted_tree_variance_bias_decomposition(dataframe, dt):
+    n_tests = 3
+    stats = np.array([])
+
+    x_train, x_valid, y_train, y_valid = train_test_split(dataframe.loc[:, dataframe.columns != 'Winner'],
+                                                          dataframe['Winner'], test_size=0.33, shuffle=False,
+                                                          stratify=None)
+
+    n_estimators = range(2, 21)
+    for n in n_estimators:
+        y_preds = np.array([])
+
+        for i in range(n_tests):
+            xs, ys = resample(x_train, y_train, n_samples=int(0.67 * len(y_train)))
+
+            # train a decision tree classifier
+            estimator = AdaBoostClassifier(dt, n_estimators=n)
+            estimator.fit(xs, ys)
+
+            y_pred = estimator.predict(x_valid)
+            y_preds = np.column_stack([y_preds, y_pred]) if y_preds.size else y_pred
+
+        est_bias = (y_valid - np.mean(y_preds, axis=1)) ** 2
+        est_variance = np.var(y_preds, axis=1)
+        est_error = (y_preds - y_valid.values.reshape(-1, 1)) ** 2
+
+        run_stats = np.array([est_error.mean(), est_bias.mean(), est_variance.mean()])
+
+        stats = np.column_stack([stats, run_stats]) if stats.size else run_stats
+
+    plot_estimator_variance_bias_decomposition(n_estimators, stats, 'Estimators')
+
+
+def get_forest_variance_bias_decomposition(dataframe, best_estimators=None):
+    n_tests = 3
+    stats = np.array([])
+
+    x_train, x_valid, y_train, y_valid = train_test_split(dataframe.loc[:, dataframe.columns != 'Winner'],
+                                                          dataframe['Winner'], test_size=0.33, shuffle=False,
+                                                          stratify=None)
+    if best_estimators is None:
+        n_estimators = range(50, 301, 25)
+        for n in n_estimators:
+            y_preds = np.array([])
+
+            for i in range(n_tests):
+                xs, ys = resample(x_train, y_train, n_samples=int(0.67 * len(y_train)))
+
+                # train a decision tree classifier
+                estimator = RandomForestClassifier(n_estimators=n, nrandom_state=13, n_jobs=-1)
+                estimator.fit(xs, ys)
+
+                y_pred = estimator.predict(x_valid)
+                y_preds = np.column_stack([y_preds, y_pred]) if y_preds.size else y_pred
+
+            est_bias = (y_valid - np.mean(y_preds, axis=1)) ** 2
+            est_variance = np.var(y_preds, axis=1)
+            est_error = (y_preds - y_valid.values.reshape(-1, 1)) ** 2
+
+            run_stats = np.array([est_error.mean(), est_bias.mean(), est_variance.mean()])
+
+            stats = np.column_stack([stats, run_stats]) if stats.size else run_stats
+
+        plot_estimator_variance_bias_decomposition(n_estimators, stats, 'Estimators')
+    else:
+        depth = range(2, 20)
+        for n in depth:
+            y_preds = np.array([])
+
+            for i in range(n_tests):
+                xs, ys = resample(x_train, y_train, n_samples=int(0.67 * len(y_train)))
+
+                # train a decision tree classifier
+                estimator = RandomForestClassifier(n_estimators=best_estimators, max_depth=n,
+                                                   random_state=13, n_jobs=-1)
+                estimator.fit(xs, ys)
+
+                y_pred = estimator.predict(x_valid)
+                y_preds = np.column_stack([y_preds, y_pred]) if y_preds.size else y_pred
+
+            est_bias = (y_valid - np.mean(y_preds, axis=1)) ** 2
+            est_variance = np.var(y_preds, axis=1)
+            est_error = (y_preds - y_valid.values.reshape(-1, 1)) ** 2
+
+            run_stats = np.array([est_error.mean(), est_bias.mean(), est_variance.mean()])
+
+            stats = np.column_stack([stats, run_stats]) if stats.size else run_stats
+
+        plot_estimator_variance_bias_decomposition(depth, stats, 'Depth')
+
+
+def plot_estimator_variance_bias_decomposition(n_inst, stats, x_label):
+    fig, ax = plt.subplots(figsize=(15, 10))
+
+    fig.suptitle('Bias$^2$-Variance Decomposition')
+
+    ax.plot(n_inst, stats[0, :], 'o:', label='Error')
+    ax.plot(n_inst, stats[1, :], 'o:', label='Bias$^2$')
+    ax.plot(n_inst, stats[2, :], 'o:', label='Variance')
+    ax.set_xlabel(x_label)
+    ax.set_ylabel('Error')
+    ax.grid()
+    ax.legend()
+
+
+'''
 def plot_estimator_variance_bias_decomposition(dataframe, estimator, n_tests=20):
     stats = np.array([])
 
@@ -48,6 +220,7 @@ def plot_estimator_variance_bias_decomposition(dataframe, estimator, n_tests=20)
     ax.set_ylabel('Error')
     ax.grid()
     ax.legend()
+'''
 
 
 def plot_estimator_accuracy(ax, accuracies, hyperparameter):
@@ -82,7 +255,7 @@ def get_best_dataframe_split(dataframe):
         return [[best_valid_accuracy, best_train_accuracy, index]]
 
     cut_dataframes = [dataframe.drop(dataframe[dataframe['csvID'].isin(range(0, i))].index.values, axis=0)
-                     for i in range(0, int(max(dataframe['csvID'])))]
+                      for i in range(0, int(max(dataframe['csvID'])))]
     accuracies = []
 
     for i, df in enumerate(cut_dataframes):
@@ -234,7 +407,8 @@ def validate_forest_classifier(dataframe):
         accuracies = []
 
         for estimators in range(50, 301, 25):
-            rf = RandomForestClassifier(n_estimators=estimators, n_jobs=-1)  # Traininig su più core n_jobs -1
+            rf = RandomForestClassifier(n_estimators=estimators, n_jobs=-1,
+                                        random_state=13)  # Traininig su più core n_jobs -1
             rf.fit(x_train, y_train)
 
             # compute Accuracy
@@ -260,7 +434,7 @@ def validate_forest_classifier(dataframe):
 
         for max_depth in range(2, 20):
             rf = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth,
-                                        n_jobs=-1)
+                                        n_jobs=-1, random_state=13)
             rf.fit(x_train, y_train)
 
             # compute Accuracy
